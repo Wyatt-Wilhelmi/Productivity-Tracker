2ae916742a0a40fc35f786c62cffedbd7066966e
Wyatt-Wilhelmi <wilhelmiw17@gmail.com>
2023-11-10 11:52:41 -0500

    Converted all event listeners to Alpine

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Your code here
-    document.getElementById('darkModeToggle').addEventListener('click', function() {
-        const body = document.body;
-    
-        if (body.classList.contains('dark')) {
-            body.classList.remove('dark');
-        } else {
-            body.classList.add('dark');
-        }
-    });
-
-    // function addItem(checklist, itemCount) {
-    //     const newItem = document.createElement('li'); // Create a new li element
-    //     newItem.textContent = `Item ${itemCount + 1}`; // Add text to the new item
-    //     checklist.appendChild(newItem); // Append the new item to the checklist
-    // }
-
-    // const addItemButtonMonday = document.getElementById('addItemButtonMonday');
-    //     addItemButtonMonday.addEventListener('click', (event) => {
-    //         event.stopPropagation(); // Prevents the click from bubbling up to parent elements
-
-    //         const checklistMonday = document.getElementById('checklistMonday');
-    //         addItem(checklistMonday, checklistMonday.children.length); // Pass the list and current item count
-    //     });
-    
-});
-
 // document.addEventListener('alpine:init', () => {
 //     Alpine.data('checklistComponent', (day) => ({
 //         day: day,
