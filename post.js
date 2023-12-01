2fa4adce0036ab5fba23a6c334b41431c23bc3fc
Wyatt-Wilhelmi <wilhelmiw17@gmail.com>
2023-11-30 20:44:51 -0500

    Edit and delete buttons now adjust appropriately

--- post.js
+++ post.js
@@ -123,7 +123,7 @@ function createButtonContainer(day, itemID, inputValue) {
     let mapDay = 1;
 
     const containerDiv = document.createElement('div');
-    containerDiv.className = 'button-container mx-1 max-w-fit dark:bg-stone-50 dark:text-black bg-gray-900 rounded-md text-white';
+    containerDiv.className = 'button-container mx-1 max-w-fit dark:bg-gray-900 bg-stone-200 rounded-md';
 
     // Add the buttons as in your example
 
@@ -131,30 +131,32 @@ function createButtonContainer(day, itemID, inputValue) {
     // Button 1
     const button1 = document.createElement('button');
     button1.id = 'editButton' + newButtonCount;
-    button1.className = 'p-1 m-0 hover:bg-white hover:bg-opacity-25 rounded-md';
+    button1.className = 'p-2 m-0 hover:bg-gray-900 dark:hover:bg-stone-200 dark:hover:text-black dark:text-white hover:text-white text-black rounded-md';
     button1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                         </svg>`;
 
     // button1.addEventListener('click', function(event){
-    //     const inputBox = document.getElementById('inputBox' + day);
-    //     inputBox.value = inputValue;
+    //     const editInputBox = document.getElementById('inputBox' + day);
+    //     editInputBox.value = inputValue;
 
-    //     const inputSection = document.getElementById('inputSection' + day);
+    //     const editInputSection = document.getElementById('inputSection' + day);
 
-    //     if(inputSection){
-    //         inputSection.classList.toggle('hidden');
-    //         inputSection.classList.toggle('flex');
+    //     const editSubmitButton = document.getElementById('submitButton' + day);
+
+    //     if(editInputSection){
+    //         editInputSection.classList.toggle('hidden');
+    //         editInputSection.classList.toggle('flex');
     //     }
 
-    //     if (inputBox) {
-    //         inputBox.addEventListener('keyup', function(event) {           
+    //     if (editInputBox) {
+    //         editInputBox.addEventListener('keyup', function(event) {           
     //             // Your event handling logic here
     //             if(event.key === 'Enter'){
 
-    //                 if(inputSection){
-    //                     inputSection.classList.toggle('flex');
-    //                     inputSection.classList.toggle('hidden');
+    //                 if(editInputSection){
+    //                     editInputSection.classList.toggle('flex');
+    //                     editInputSection.classList.toggle('hidden');
     //                 }                            
 
     //             const payload = {
@@ -166,18 +168,18 @@ function createButtonContainer(day, itemID, inputValue) {
     //             }
 
 
-    //             inputBox.value = '';
+    //             editInputBox.value = '';
                     
     //             }
     //         });
     //     }
 
-    //     if(submitButton){
-    //         submitButton.addEventListener('click', function(event) {
+    //     if(editSubmitButton){
+    //         editSubmitButton.addEventListener('click', function(event) {
 
-    //             if(inputSection){
-    //                 inputSection.classList.toggle('flex');
-    //                 inputSection.classList.toggle('hidden');
+    //             if(editInputSection){
+    //                 editInputSection.classList.toggle('flex');
+    //                 editInputSection.classList.toggle('hidden');
     //             }
 
     //            const payload = {
@@ -190,7 +192,7 @@ function createButtonContainer(day, itemID, inputValue) {
 
 
 
-    //             inputBox.value = '';
+    //             editInputBox.value = '';
                 
     //         });
     //     } 
@@ -202,7 +204,7 @@ function createButtonContainer(day, itemID, inputValue) {
     // Button 2
     const button2 = document.createElement('button');
     button2.id = 'deleteButton' + newButtonCount;
-    button2.className = 'p-1 m-0 hover:bg-white hover:bg-opacity-25 rounded-md';
+    button2.className = 'p-2 m-0 hover:bg-gray-900 dark:hover:bg-stone-200 dark:hover:text-black dark:text-white hover:text-white text-black rounded-md';
     button2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
     </svg>`;
@@ -228,7 +230,7 @@ function populateToDoListItems(){
     for (const item of newToDoListItems) {
         // Create the main wrapper div
         const wrapperDiv = document.createElement('div');
-        wrapperDiv.className = 'checkbox-wrapper-52 flex flex-wrap';
+        wrapperDiv.className = 'checkbox-wrapper-52 self-start w-auto';
         // wrapperDiv.classList.add('text-xl');
 
         // Create the label that wraps everything
@@ -294,7 +296,8 @@ function populateToDoListItems(){
 
         // Append the wrapper div to the correct day list
             if (!checklistMap[item.day]) {
-                checklistMap[item.day] = document.createElement('ul');
+                checklistMap[item.day] = document.createElement('div');
+                checklistMap[item.day].className = 'flex flex-col';
             }
         checklistMap[item.day].appendChild(wrapperDiv);
     }
